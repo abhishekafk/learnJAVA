7327fec4952b6352000f9acf73f4eea098ac5713
Abhishek <abhishekmadaan567@gmail.com>
2025-09-16 01:01:25 +0530

    Initial commit for learnJAVA project

---
+++ ip.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER YOUR NUMBER");
+        int a= sc.nextInt();
+System.out.print("YOUR NUMBER-" + a);
+sc.close();
+    }
+}
